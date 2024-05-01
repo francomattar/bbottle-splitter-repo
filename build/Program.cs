bf2d7eee2bea400d67d7f07e7023930322add92f
francomattar <mattarfranco3@gmail.com>
2024-05-01 16:09:27 +0100

    fix building

---
+++ build/Program.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using GlobExpressions;
+using static Bullseye.Targets;
+using static SimpleExec.Command;
+
+const string Clean = "clean";
+const string Restore = "restore";
+const string Build = "build";
+const string Test = "test";
+const string Format = "format";
+const string Publish = "publish";
+
+Target(
+    Clean,
+    ForEach("**/bin", "**/obj"),
+    dir =>
+    {
+        IEnumerable<string> GetDirectories(string d)
+        {
+            return Glob.Directories(".", d);
+        }
+
+        void RemoveDirectory(string d)
+        {
+            if (Directory.Exists(d))
+            {
+                Console.WriteLine(d);
+                Directory.Delete(d, true);
+            }
+        }
+
+        foreach (var d in GetDirectories(dir))
+        {
+            RemoveDirectory(d);
+        }
+    }
+);
+
+Target(
+    Format,
+    () =>
+    {
+        Run("dotnet", "tool restore");
+        Run("dotnet", "csharpier --check .");
+    }
+);
+Target(Restore, DependsOn(Format), () => Run("dotnet", "restore"));
+
+Target(
+    Build,
+    DependsOn(Restore),
+    () =>
+    {
+        Run("dotnet", "build src/BottleSplitter/BottleSplitter.csproj -c Release --no-restore --output ./output");
+    }
+);
+
+Target(
+    Test,
+    DependsOn(Build),
+    () =>
+    {
+        IEnumerable<string> GetFiles(string d)
+        {
+            return Glob.Files(".", d);
+        }
+
+        foreach (var file in GetFiles("**/*.Test.csproj"))
+        {
+            Run("dotnet", $"test {file} -c Release -f net8.0 --no-restore --verbosity=normal");
+        }
+    }
+);
+
+Target("default", DependsOn(Publish), () => Console.WriteLine("Done!"));
+
+await RunTargetsAndExitAsync(args);
